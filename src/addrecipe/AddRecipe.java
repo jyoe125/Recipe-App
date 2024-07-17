d8c4920fff193d26499ad41b1c484d75bed85af4
shuej <shuej@CSJ>
2024-07-17 12:23:28 +0800

    completed add recipes

---
+++ src/addrecipe/AddRecipe.java
@@ -0,0 +1,13 @@
+package addrecipe;
+
+
+public class AddRecipe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("App recipes");
+    }
+    
+}
